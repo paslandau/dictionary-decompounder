2919a63725741d917414f77004b998997b27190f
paslandau <pascal.landau@googlemail.com>
2014-11-22 22:21:44 +0100

    Added license

--- demo-filter.php
+++ demo-filter.php
@@ -7,19 +7,19 @@ use paslandau\DictionaryDecompounder\Interfix\Interfixer;
 
 require_once __DIR__ . '/demo-bootstrap.php';
 
-// Create dictionary
 $dictionary = new ArrayDictionary();
-$dictionary->addWords(["Straße","Sperre","Foo","Bar"]);
+$dictionary->addWords(["Don","Au", "Donau","Dampf","Schiff","Fahrt", "Gesellschaft","Kapitän"]);
 
 // Define interfixes - this is language specific!
-$interfixes = array("n");
+$interfixes = array("s");
 $interfixer = new Interfixer($interfixes);
 
 // Create decompounder
 $decompounder = new Decompounder($dictionary, $interfixer);
 
 // Decompound
-$word = "Straßensperre";
+$word = "donaudampfschifffahrtsgesellschaftskapitän";
 $result = $decompounder->decompoundWord($word);
-
-echo $result;
+$filter = new DecompoundFilter(false,true,true);
+$res = $filter->filterBest($result);
+echo implode("-",$res);
