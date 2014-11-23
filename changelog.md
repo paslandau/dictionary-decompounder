0296a46e90c83c32489d3308707e1a3b11f9cb57
paslandau <pascal.landau@googlemail.com>
2014-11-23 11:59:04 +0100

    license and autoloading fixed

--- changelog.md
+++ changelog.md
@@ -1,3 +1,8 @@
+###0.0.2
+
+- added license
+- Fixed demo* files autoloading
+
 ###0.0.1
 
 - Initial commit
--- demo-bootstrap.php
+++ demo-bootstrap.php
@@ -5,7 +5,7 @@ mb_internal_encoding("utf-8");
 
 //discover autoloader
 $path = "vendor/autoload.php";
-for($i=0; $i<3;$i++){
+for($i=0; $i<4;$i++){
     $depth = "/";
     for($j=0;$j<$i;$j++){
         $depth .= "../";
